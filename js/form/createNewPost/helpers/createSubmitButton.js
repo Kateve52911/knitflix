71aeb578fee8d55f3799705dae28677532511857
Kateve52911 <kateve52911@stud.noroff.no>
2025-04-27 10:43:35 +0200

    updatet button stylings and created a button component

--- js/form/createNewPost/helpers/createSubmitButton.js
+++ js/form/createNewPost/helpers/createSubmitButton.js
@@ -2,7 +2,6 @@ export function createSubmitButton(text) {
   const button = document.createElement("button");
   button.type = "submit";
   button.textContent = text;
-  button.className =
-    "bg-accent-primaryBtn py-2 px-4 rounded-md text-white font-body";
+  button.className = "bg-accent-primaryBtn btn text-white font-body";
   return button;
 }
